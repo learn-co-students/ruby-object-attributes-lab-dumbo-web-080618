b719b4c95707354753a2e38d55461b50f17463d1
Madeline Celis <celiscb@vcu.edu>
2018-08-07 18:44:55 -0400

    complete

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+
+  #setter function
+  def name=(person_name)
+    @name = person_name
+  end
+
+  #getter function
+  def name
+    @name
+  end
+
+#setter function
+  def job=(persons_job)
+    @job = persons_job
+  end
+
+  def job
+    @job
+  end
+end
